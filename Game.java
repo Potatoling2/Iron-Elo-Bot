1011335c485cc351d6b2235d82bb2e951fff31e5
Potatoling2 <116038546+Potatoling2@users.noreply.github.com>
2023-01-06 10:02:13 -0800

    Update Game.java

--- Game.java
+++ Game.java
@@ -10,12 +10,18 @@ public class Game {
         return (b1 + s1) - (b2 + s2);
     }
     public static void gold(int diff) {
+        int team = 1;
         if(diff > 7) {
             diff = 7;
         }
         int bound = 50 + diff * 7;
         int factor = bound - Rand(1,100);
         //incomplete
+        int lead = factor * 50;
+        if(lead < 0) {
+            lead = lead * -1;
+            team++;
+        }
         System.out.println("");
     }
     public static void objective() {
