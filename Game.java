5e683fe3b697a013f433982c7961eba4c30e1576
Potatoling2 <116038546+Potatoling2@users.noreply.github.com>
2023-01-10 14:15:29 -0800

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,7 +1,37 @@
 import java.util.*;
 public class Game {
-    public static int Rand(int min, int max){
+    public static int Rand(int min, int max) {
         return (int)(Math.random() * (max - min + 1)) + min;
+    }
+    public static int glead(int diff) {
+        int bound = 50 + diff * 7;
+        int factor = bound - Rand(1,100);
+        int lead = factor * 50;
+        return lead;
+    }
+    public static int pcheck(int diff) {
+        int team = 0;
+        if(diff > 7) {
+            diff = 7;
+        }
+        int bound = 50 + diff * 7;
+        int check = bound - Rand(1,100);
+        if(check > 0) {
+            team = 1;
+        }
+        else if(check < 0) {
+            team = -1;
+        }
+        else if(check == 0) {
+            int tiecheck = Rand(1,100);
+            if(check > 50) {
+                team = 1;
+            }
+            else if(check < 50) {
+                team = -1;
+            }
+        }
+        return team;
     }
     public static int solorole(int p1, int p2) {
         return p1 - p2;
@@ -10,12 +40,7 @@ public class Game {
         return (b1 + s1) - (b2 + s2);
     }
     public static int gold(int diff, String team1, String team2) {
-        if(diff > 7) {
-            diff = 7;
-        }
-        int bound = 50 + diff * 7;
-        int factor = bound - Rand(1,100);
-        int lead = factor * 50;
+        int lead = glead(diff);
         if(lead < 0) {
             lead = lead * -1;
             System.out.println(lead + " gold lead for " + team2);
@@ -30,7 +55,17 @@ public class Game {
         System.out.println("");
         return lead;
     }
-    public static void objective() {
+    public static int objective(int lead, int jgdiff, String team1, String team2) {
+        int golddiff = lead / 2000;
+        int diff = golddiff + jgdiff;
+        int objdiff = pcheck(diff);
+        if(objdiff < 0) {
+            System.out.println("Objective for " + team2);
+        }
+        else if(objdiff > 0) {
+            System.out.println("Objective for " + team1);
+        }
+        return objdiff;
     }
     public static void win() {
     }
